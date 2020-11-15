c3e82321d6abc8623687d08a605442ebf2c6924e
jcgentr <jcgentr@clemson.edu>
2020-11-15 17:59:28 -0500

    trigger change in github

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ import Header from "../components/header"
 
 export default function Contact() {
   return (
-    <div style={{ color: `blue` }}>
+    <div style={{ color: `teal` }}>
       <Link to="/">Home</Link>
       <Header headerText="Contact" />
       <p>Send us a message!</p>
